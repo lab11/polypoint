d3d1d9c71aa2975f267751efd0e350c85ec0c51a
Brad Campbell <bradjc5@gmail.com>
2015-09-17 13:19:05 -0400

    improve script to read from ble

--- phone/tritag.js
+++ phone/tritag.js
@@ -1,7 +1,10 @@
 var noble = require('noble');
 var loc = require('./localization');
+var buf = require('buffer');
 
-TRITAG_SERVICE_UUID = 'd68c3152a23fee900c455231395e5d2e';
+var TRITAG_SERVICE_UUID = 'd68c3152a23fee900c455231395e5d2e';
+
+var TRIPOINT_READ_INT_RANGES = 1
 
 var anchors = {
 	one: [0,0,0],
@@ -20,6 +23,27 @@ var test1 = {
 var actual = {
 	test1: [3.32, 2.16, 0],
 };
+
+function buf_to_eui (b, offset) {
+	var eui = '';
+	for (var i=0; i<8; i++) {
+		var val = b.readUInt8(offset+i);
+		var val = val.toString(16);
+		if (val.length == 1) {
+			val = '0' + val;
+		}
+		eui = val+eui;
+		if (i<7) {
+			eui = ':' + eui;
+		}
+	}
+	return eui;
+}
+
+function encoded_mm_to_meters (b, offset) {
+	var mm = b.readInt32LE(offset);
+	return mm / 1000.0;
+}
 
 noble.on('stateChange', function (state) {
 	if (state === 'poweredOn') {
@@ -42,6 +66,45 @@ noble.on('discover', function (peripheral) {
 
 				services[0].discoverCharacteristics([], function (err, characteristics) {
 					console.log('Found ' + characteristics.length + ' characteristics');
+
+					characteristics.forEach(function (el, idx, arr) {
+						var characteristic = el;
+
+						if (characteristic.uuid == 'd68c3153a23fee900c455231395e5d2e') {
+
+							function get_range () {
+								characteristic.read(function (err, data) {
+									console.log(data);
+
+
+									var reason = data.readUInt8(0);
+									if (reason == TRIPOINT_READ_INT_RANGES) {
+										// Got ranges from the TAG.
+										var num_ranges = data.readUInt8(1);
+										if (num_ranges == 0) {
+											console.log('No anchors in range.');
+										} else {
+											// Iterate the array to get the
+											// anchor IDs and ranges.
+											var offset_start = 2;
+											var instance_length = 12;
+											for (var i=0; i<num_ranges; i++) {
+												var start = offset_start + (i*instance_length);
+												var eui = buf_to_eui(data, start);
+												var range = encoded_mm_to_meters(data, start+8);
+												console.log(eui);
+												console.log(range);
+											}
+										}
+									}
+								});
+							}
+
+							get_range();
+							setInterval(get_range, 1000);
+						}
+					});
+
 				});
 
 			}
