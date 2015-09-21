63af172c49f27d92e995daafa28a3d64ec2cfaeb
Brad Campbell <bradjc5@gmail.com>
2015-09-20 23:01:43 -0400

    switched to simple_ble
    removed a bunch of code that doesn't need to be copied around anymore

--- nrf51822/apps/tritag/ble_config.h
+++ nrf51822/apps/tritag/ble_config.h
@@ -80,7 +80,6 @@
 
 
 typedef struct ble_app_s {
-    uint16_t                     conn_handle;           /**< Handle of the current connection (as provided by the S110 SoftDevice). This will be BLE_CONN_HANDLE_INVALID when not in a connection. */
     // uint16_t                     revision;           /**< Handle of DFU Service (as provided by the S110 SoftDevice). */
     uint16_t                     service_handle;        /**< Handle of DFU Service (as provided by the S110 SoftDevice). */
     uint8_t                      uuid_type;             /**< UUID type assigned for DFU Service by the S110 SoftDevice. */
--- nrf51822/apps/tritag/main.c
+++ nrf51822/apps/tritag/main.c
@@ -28,6 +28,9 @@
 #include "app_util.h"
 #include "app_timer.h"
 
+#include "simple_ble.h"
+#include "eddystone.h"
+
 #include "led.h"
 #include "boards.h"
 
@@ -51,23 +54,13 @@ const ble_uuid128_t tritag_uuid128 = {
 // UUID for the TriTag service
 ble_uuid_t tritag_uuid;
 
-// Security requirements for this application.
-static ble_gap_sec_params_t m_sec_params = {
-    SEC_PARAM_BOND,
-    SEC_PARAM_MITM,
-    SEC_PARAM_IO_CAPABILITIES,
-    SEC_PARAM_OOB,
-    SEC_PARAM_MIN_KEY_SIZE,
-    SEC_PARAM_MAX_KEY_SIZE,
-};
 
 /*******************************************************************************
  *   State for this application
  ******************************************************************************/
  // Main application state
+simple_ble_app_t* simple_ble_app;
 static ble_app_t app;
-
-static ble_gap_adv_params_t m_adv_params;
 
 // GP Timer. Used to retry initializing the TriPoint.
 static app_timer_id_t  app_timer;
@@ -78,79 +71,13 @@ bool tripoint_inited = false;
 
 
 
-static void advertising_start (void);
-static void advertising_stop (void);
-
-/*******************************************************************************
- *   nRF Error Functions
- ******************************************************************************/
-
-/**@brief Function for error handling, which is called when an error has occurred.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyze
- *          how your product is supposed to react in case of error.
- *
- * @param[in] error_code  Error code supplied to the handler.
- * @param[in] line_num    Line number where the handler is called.
- * @param[in] p_file_name Pointer to the file name.
- */
-void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
-{
-    // APPL_LOG("[APPL]: ASSERT: %s, %d, error 0x%08x\r\n", p_file_name, line_num, error_code);
-    // nrf_gpio_pin_set(ASSERT_LED_PIN_NO);
-
-    // This call can be used for debug purposes during development of an application.
-    // @note CAUTION: Activating this code will write the stack to flash on an error.
-    //                This function should NOT be used in a final product.
-    //                It is intended STRICTLY for development/debugging purposes.
-    //                The flash write will happen EVEN if the radio is active, thus interrupting
-    //                any communication.
-    //                Use with care. Un-comment the line below to use.
-    // ble_debug_assert_handler(error_code, line_num, p_file_name);
-
-    // On assert, the system can only recover with a reset.
-	//volatile uint8_t i = 5;
-
-	/*for(uint8_t i = 0; i < error_code; i++) {
-		led_off(LED_0);
-		led_toggle(LED_0);
-		nrf_delay_us(10000);
-		led_toggle(LED_0);
-		nrf_delay_us(1000000);
-	}*/
-
-    //NVIC_SystemReset();
-}
-
-
-/**@brief Function for asserts in the SoftDevice.
- *
- * @details This function will be called in case of an assert in the SoftDevice.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyze
- *          how your product is supposed to react in case of Assert.
- * @warning On assert from the SoftDevice, the system can only recover on reset.
- *
- * @param[in] line_num     Line number of the failing ASSERT call.
- * @param[in] p_file_name  File name of the failing ASSERT call.
- */
-void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name) {
-    app_error_handler(0xDEADBEEF, line_num, p_file_name);
-}
-
-// Service error callback
-static void service_error_handler(uint32_t nrf_error) {
-    APP_ERROR_HANDLER(nrf_error);
-}
-
-
 
 /*******************************************************************************
  *   nRF CALLBACKS - In response to various BLE/hardware events.
  ******************************************************************************/
 
 // Function for handling the WRITE CHARACTERISTIC BLE event.
-static void on_write (ble_evt_t* p_ble_evt) {
+void ble_evt_write (ble_evt_t* p_ble_evt) {
     ble_gatts_evt_write_t* p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
 
     // Handle a write to the characteristic that starts and stops
@@ -171,142 +98,6 @@ static void on_write (ble_evt_t* p_ble_evt) {
 }
 
 
-// Connection parameters event handler callback
-static void on_conn_params_evt (ble_conn_params_evt_t * p_evt) {
-    uint32_t err_code;
-
-    if (p_evt->evt_type == BLE_CONN_PARAMS_EVT_FAILED) {
-        err_code = sd_ble_gap_disconnect(app.conn_handle,
-                                         BLE_HCI_CONN_INTERVAL_UNACCEPTABLE);
-        APP_ERROR_CHECK(err_code);
-    }
-}
-
-// Connection parameters error callback
-static void conn_params_error_handler(uint32_t nrf_error) {
-    APP_ERROR_HANDLER(nrf_error);
-}
-
-
-
-
-
-/**@brief Function for handling the Application's BLE Stack events.
- *
- * @param[in]   p_ble_evt   Bluetooth stack event.
- */
-static void on_ble_evt(ble_evt_t * p_ble_evt) {
-    uint32_t                         err_code;
-    static ble_gap_evt_auth_status_t m_auth_status;
-
-    switch (p_ble_evt->header.evt_id) {
-        case BLE_GAP_EVT_CONNECTED:
-            app.conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
-            led_on(LED_0);
-            //advertising_stop();
-            break;
-
-        case BLE_GAP_EVT_DISCONNECTED:
-            app.conn_handle = BLE_CONN_HANDLE_INVALID;
-            advertising_start();
-            led_off(LED_0);
-            break;
-
-        case BLE_GATTS_EVT_WRITE:
-            on_write(p_ble_evt);
-            break;
-
-        case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
-            err_code = sd_ble_gap_sec_params_reply(app.conn_handle,
-                                                   BLE_GAP_SEC_STATUS_SUCCESS,
-                                                   &m_sec_params,
-                                                   NULL);
-            APP_ERROR_CHECK(err_code);
-            break;
-
-        case BLE_GATTS_EVT_SYS_ATTR_MISSING:
-            err_code = sd_ble_gatts_sys_attr_set(app.conn_handle, NULL, 0, 0);
-            APP_ERROR_CHECK(err_code);
-            break;
-
-        case BLE_GAP_EVT_AUTH_STATUS:
-            m_auth_status = p_ble_evt->evt.gap_evt.params.auth_status;
-            break;
-
-        case BLE_GAP_EVT_SEC_INFO_REQUEST:
-            // No keys found for this device.
-            err_code = sd_ble_gap_sec_info_reply(app.conn_handle, NULL, NULL, NULL);
-            APP_ERROR_CHECK(err_code);
-            break;
-
-        case BLE_GAP_EVT_TIMEOUT:
-            if (p_ble_evt->evt.gap_evt.params.timeout.src == BLE_GAP_TIMEOUT_SRC_ADVERTISING) {
-                err_code = sd_power_system_off();
-                APP_ERROR_CHECK(err_code);
-            }
-            break;
-
-        default:
-            break;
-    }
-}
-
-// /**@brief Function for handling the Application's system events.
-//  *
-//  * @param[in]   sys_evt   system event.
-//  */
-// static void on_sys_evt(uint32_t sys_evt)
-// {
-//     switch(sys_evt)
-//     {
-//         case NRF_EVT_FLASH_OPERATION_SUCCESS:
-//         case NRF_EVT_FLASH_OPERATION_ERROR:
-//             if (m_memory_access_in_progress)
-//             {
-//                 m_memory_access_in_progress = false;
-//                 scan_start();
-//             }
-//             break;
-//         default:
-//             // No implementation needed.
-//             break;
-//     }
-// }
-
-
-/**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
- *
- * @details This function is called from the scheduler in the main loop after a BLE stack event has
- *  been received.
- *
- * @param[in]   p_ble_evt   Bluetooth stack event.
- */
-static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
-{
-   // dm_ble_evt_handler(p_ble_evt);
-   // ble_db_discovery_on_ble_evt(&m_ble_db_discovery, p_ble_evt);
-   // ble_hrs_c_on_ble_evt(&m_ble_hrs_c, p_ble_evt);
-   // ble_bas_c_on_ble_evt(&m_ble_bas_c, p_ble_evt);
-    on_ble_evt(p_ble_evt);
-    ble_conn_params_on_ble_evt(p_ble_evt);
-}
-
-
-/**@brief Function for dispatching a system event to interested modules.
- *
- * @details This function is called from the System event interrupt handler after a system
- *          event has been received.
- *
- * @param[in]   sys_evt   System stack event.
- */
-static void sys_evt_dispatch(uint32_t sys_evt)
-{
-    // pstorage_sys_event_handler(sys_evt);
-    // on_sys_evt(sys_evt);
-}
-
-
-
 
 /*******************************************************************************
  *   PolyPoint Callbacks
@@ -316,12 +107,11 @@ uint8_t updated = 0;
 uint32_t blobLen;
 uint8_t dataBlob[256];
 
-void updateData( uint8_t * data, uint32_t len) {
-	if(len < 256) {
+void updateData (uint8_t * data, uint32_t len) {
+	if (len < 256) {
 		memcpy(app.app_raw_response_buffer, data, len);
 		blobLen = len;
-	}
-	else {
+	} else {
 		memcpy(app.app_raw_response_buffer, data, 256);
 		blobLen = 256;
 	}
@@ -335,18 +125,18 @@ void tripointDataUpdate () {
 		led_toggle(LED_0);
 	}
 
-	if(app.conn_handle != BLE_CONN_HANDLE_INVALID) {
+	if(simple_ble_app->conn_handle != BLE_CONN_HANDLE_INVALID) {
 
 		ble_gatts_hvx_params_t notify_params;
 		uint16_t len = blobLen;
 		notify_params.handle = app.char_location_handles.value_handle;
-		notify_params.type   =  BLE_GATT_HVX_NOTIFICATION;
-		notify_params.offset =  0;
-		notify_params.p_len  =  &len;
-		notify_params.p_data =  app.app_raw_response_buffer;
-		
+		notify_params.type   = BLE_GATT_HVX_NOTIFICATION;
+		notify_params.offset = 0;
+		notify_params.p_len  = &len;
+		notify_params.p_data = app.app_raw_response_buffer;
+
 		volatile uint32_t err_code = 0;
-		err_code = sd_ble_gatts_hvx(app.conn_handle, &notify_params);
+		err_code = sd_ble_gatts_hvx(simple_ble_app->conn_handle, &notify_params);
 	}
 
 	updated = 0;
@@ -367,51 +157,14 @@ static void timer_handler (void* p_context) {
 
 
 
-/*******************************************************************************
- *   BLE OPERATIONS and FUNCTIONS
- ******************************************************************************/
-
-
-
-// Tell the nrf to start advertising
-static void advertising_start (void) {
-    uint32_t err_code = sd_ble_gap_adv_start(&m_adv_params);
-    APP_ERROR_CHECK(err_code);
-}
-
-static void advertising_stop (void) {
-    uint32_t err_code = sd_ble_gap_adv_stop();
-    APP_ERROR_CHECK(err_code);
-}
-
-
 
 
 /*******************************************************************************
  *   INIT FUNCTIONS
  ******************************************************************************/
 
-// Initialize connection parameters
-static void conn_params_init(void) {
-    uint32_t               err_code;
-    ble_conn_params_init_t cp_init;
 
-    memset(&cp_init, 0, sizeof(cp_init));
-
-    cp_init.p_conn_params                  = NULL;
-    cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
-    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = false;
-    cp_init.evt_handler                    = on_conn_params_evt;
-    cp_init.error_handler                  = conn_params_error_handler;
-
-    err_code = ble_conn_params_init(&cp_init);
-    APP_ERROR_CHECK(err_code);
-}
-
-static void timers_init(void) {
+static void timers_init (void) {
     uint32_t err_code;
 
     APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, false);
@@ -425,43 +178,9 @@ static void timers_init(void) {
     APP_ERROR_CHECK(err_code);
 }
 
-// initialize advertising
-static void advertising_init(void) {
-    uint32_t      err_code;
-    ble_advdata_t advdata;
-    ble_advdata_t srdata;
-    uint8_t       flags =  BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
-
-    // Advertise our custom service
-    ble_uuid_t adv_uuids[] = {tritag_uuid};
-
-    // Build and set advertising data
-    memset(&advdata, 0, sizeof(advdata));
-    memset(&srdata, 0, sizeof(srdata));
-
-    advdata.name_type               = BLE_ADVDATA_NO_NAME;
-    advdata.include_appearance      = true;
-    advdata.flags                   = flags;
-    advdata.uuids_complete.uuid_cnt = sizeof(adv_uuids) / sizeof(adv_uuids[0]);
-    advdata.uuids_complete.p_uuids  = adv_uuids;
-
-    // Put the name in the SCAN RESPONSE data
-    srdata.name_type                = BLE_ADVDATA_FULL_NAME;
-
-    err_code = ble_advdata_set(&advdata, &srdata);
-    APP_ERROR_CHECK(err_code);
-
-    memset(&m_adv_params, 0, sizeof(m_adv_params));
-
-    m_adv_params.type               = BLE_GAP_ADV_TYPE_ADV_IND;
-    m_adv_params.p_peer_addr        = NULL;
-    m_adv_params.fp                 = BLE_GAP_ADV_FP_ANY;
-    m_adv_params.interval           = APP_ADV_INTERVAL;
-    m_adv_params.timeout            = APP_ADV_TIMEOUT_IN_SECONDS;
-}
 
 // Init services
-static void services_init (void) {
+void services_init (void) {
     volatile uint32_t err_code;
 
     // Setup our long UUID so that nRF recognizes it. This is done by
@@ -636,90 +355,26 @@ static void services_init (void) {
 }
 
 
-// gap name/appearance/connection parameters
-static void gap_params_init (void) {
-    uint32_t                err_code;
-    ble_gap_conn_params_t   gap_conn_params;
-    ble_gap_conn_sec_mode_t sec_mode;
-
-    // Full strength signal
-    sd_ble_gap_tx_power_set(4);
-
-    // Let anyone connect and set the name given the platform
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-    err_code = sd_ble_gap_device_name_set(&sec_mode,
-                                          (const uint8_t *)DEVICE_NAME,
-                                          strlen(DEVICE_NAME));
-    APP_ERROR_CHECK(err_code);
-
-    // Not sure what this is useful for, but why not set it
-    err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_GENERIC_COMPUTER);
-    APP_ERROR_CHECK(err_code);
-
-    // Specify parameters for a connection
-    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
-    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
-    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-    gap_conn_params.slave_latency     = SLAVE_LATENCY;
-    gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
-
-    err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for initializing the BLE stack.
- *
- * @details Initializes the SoftDevice and the BLE event interrupt.
- */
-static void ble_stack_init (void) {
-    uint32_t err_code;
-
-    // Initialize the SoftDevice handler module.
-    SOFTDEVICE_HANDLER_INIT(
-        NRF_CLOCK_LFCLKSRC_RC_250_PPM_8000MS_CALIBRATION, false);
-
-    // Enable BLE stack
-    ble_enable_params_t ble_enable_params;
-    memset(&ble_enable_params, 0, sizeof(ble_enable_params));
-    ble_enable_params.gatts_enable_params.service_changed =
-                                            IS_SRVC_CHANGED_CHARACT_PRESENT;
-    err_code = sd_ble_enable(&ble_enable_params);
-    APP_ERROR_CHECK(err_code);
-
-    //Register with the SoftDevice handler module for BLE events.
-    err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
-    APP_ERROR_CHECK(err_code);
-
-    // Register with the SoftDevice handler module for BLE events.
-    err_code = softdevice_sys_evt_handler_set(sys_evt_dispatch);
-    APP_ERROR_CHECK(err_code);
-
-    // Set the MAC address of the device
-    {
-        ble_gap_addr_t gap_addr;
-
-        // Get the current original address
-        sd_ble_gap_address_get(&gap_addr);
-
-        // Set the new BLE address with the Michigan OUI
-        gap_addr.addr_type = BLE_GAP_ADDR_TYPE_PUBLIC;
-        memcpy(gap_addr.addr+2, MAC_ADDR+2, sizeof(gap_addr.addr)-2);
-        err_code = sd_ble_gap_address_set(BLE_GAP_ADDR_CYCLE_MODE_NONE, &gap_addr);
-        APP_ERROR_CHECK(err_code);
-    }
-}
-
-/** @brief Function for the Power manager.
- */
-static void power_manage (void) {
+// Function for the Power manager.
+void power_manage (void) {
     uint32_t err_code = sd_app_evt_wait();
     APP_ERROR_CHECK(err_code);
 }
 
+#define PHYSWEB_URL "goo.gl/XMRl3M"
+
+// Intervals for advertising and connections
+static const simple_ble_config_t ble_config = {
+    // c0:98:e5:45:xx:xx
+    .platform_id       = 0x45,              // used as 4th octect in device BLE address
+    .adv_name          = DEVICE_NAME,       // used in advertisements if there is room
+    .adv_interval      = MSEC_TO_UNITS(1000, UNIT_0_625_MS),
+    .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
+    .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS),
+};
 
 
-int main(void) {
+int main (void) {
     uint32_t err_code;
 
     //
@@ -731,14 +386,18 @@ int main(void) {
     // We default to doing ranging at the start
     app.app_ranging = 1;
 
-    // Setup BLE and services
-	app.conn_handle = BLE_CONN_HANDLE_INVALID;
-    ble_stack_init();
-    gap_params_init();
-    services_init();
-    advertising_init();
+    // Setup BLE
+    simple_ble_app = simple_ble_init(&ble_config);
+
+    // Setup the advertisement to use the Eddystone format.
+    // We include the device name in the scan response
+    ble_advdata_t srdata;
+    memset(&srdata, 0, sizeof(srdata));
+    srdata.name_type = BLE_ADVDATA_FULL_NAME;
+    eddystone_adv(PHYSWEB_URL, &srdata);
+
+    // Need a timer to make sure we have inited the tripoint
     timers_init();
-    conn_params_init();
 
     // Init the nRF hardware to work with the tripoint module.
     err_code = tripoint_hw_init();
@@ -750,9 +409,6 @@ int main(void) {
         tripoint_inited = true;
     }
 
-    // Advertise that we are a TORCH board
-    advertising_start();
-
     // Start the ranging!!!
     if (tripoint_inited) {
         tripoint_start_ranging(true, 10);
@@ -762,7 +418,7 @@ int main(void) {
 
     while (1) {
         power_manage();
-		if(updated) {
+		if (updated) {
 			tripointDataUpdate();
 		}
     }
