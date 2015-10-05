e05f29233005d2069634f50aee0082691247570a
Pat Pannuto <pat.pannuto@gmail.com>
2015-10-05 13:21:46 -0400

    Finish calibration script. Closes #4.

--- calibration/calibration_compute.py
+++ calibration/calibration_compute.py
@@ -4,6 +4,7 @@ import glob
 import json
 import numpy as np
 import os
+import pprint
 import sys
 
 import dataprint
@@ -13,11 +14,17 @@ if len(sys.argv) != 2:
 	sys.exit(1)
 
 condensed = sys.argv[1]
+outfilename_base = os.path.splitext(condensed)[0]
 
 meta = os.path.splitext(condensed)[0] + '.meta'
+meta = json.loads(open(meta).read())
 
 SPEED_OF_LIGHT = 299792458
 DWT_TIME_UNITS = (1.0/499.2e6/128.0) #!< = 15.65e-12 s
+
+CALIBRATION_NUM_NODES=3
+CALIB_NUM_ANTENNAS=3
+CALIB_NUM_CHANNELS=3
 
 
 def dwtime_to_dist(dwtime):
@@ -44,12 +51,22 @@ out = [
 		('round', 'calibration', 'deltaB', 'epsilonC', 'converted'),
 		]
 
+# { 'NodeA': {(ant,ch): cal, (ant,ch): cal, ...}, 'NodeB': ... }
+calibration = {'A': {}, 'B': {}, 'C': {}}
+
 for line in open(condensed):
 	try:
 		round_num, node, letters = line.split(maxsplit=2)
+		round_num = int(round_num)
 		L, M, N, O, P, Q = map(int, letters.split())
 	except ValueError:
 		continue
+
+	antenna = int(round_num / CALIBRATION_NUM_NODES) % CALIB_NUM_ANTENNAS
+	channel = int(int(round_num / CALIBRATION_NUM_NODES) / CALIB_NUM_ANTENNAS) % CALIB_NUM_CHANNELS
+	node_cal = calibration[node]
+	if (antenna,channel) not in node_cal:
+		node_cal[(antenna,channel)] = []
 
 	k = sub_dw_ts(Q,O) / sub_dw_ts(P,N)
 	deltaB = sub_dw_ts(O,L)
@@ -57,34 +74,40 @@ for line in open(condensed):
 
 	cal = deltaB - epsilonC * k - l
 
-	out.append((round_num, cal, deltaB, epsilonC, epsilonC*k))
+	node_cal[(antenna,channel)].append(cal)
 
-	if cal < 0:
-		print(dataprint.to_string(out))
-		raise
 
-print(dataprint.to_string(out))
+# http://stackoverflow.com/questions/11686720/is-there-a-numpy-builtin-to-reject-outliers-from-a-list
+def reject_outliers(data, m=2.):
+	d = np.abs(data - np.median(data))
+	mdev = np.median(d)
+	s = d/mdev if mdev else 0.
+	return data[s<m]
 
-#
-#outdata = []
-#
-#outdata.append(['RoundNum',
-#                'ATX0', 'ARX1', 'ARX2',
-#                'BRX0', 'BTX1', 'BRX2',
-#                'CRX0', 'CTX1', 'CRX2',
-#                'DRX0', 'DRX1', 'DTX2'])
-#
-#for key in sorted(data):
-#	if -1 in data[key]:
-#		continue
-#	outdata.append([key] + data[key])
-#
-#
-#outfilename_base = 'tripoint_calibration_' + timestamp
-#
-#
-#with open(outfilename_base + '.meta', 'w') as f:
-#	f.write(json.dumps(meta))
-#
-#
-#dataprint.to_newfile(outfilename_base+'.condensed', outdata, overwrite=True)
+for node in ('A', 'B', 'C'):
+	for conf in calibration[node]:
+		cal = np.array(calibration[node][conf])
+		rej = reject_outliers(cal)
+
+		if (np.std(rej) > 1000):
+			print('WARN: Bad calibration for node {} conf {}'.format(node,conf))
+			print(cal)
+			print(rej)
+			calibration[node][conf] = -1
+		else:
+			calibration[node][conf] = np.mean(rej)
+
+pprint.pprint(calibration)
+
+outdata = []
+header = ['Node ID',]
+header.extend(map(str, calibration['A'].keys()))
+outdata.append(header)
+
+for node in (('A','0'), ('B','1'), ('C','2')):
+	row = [meta[node[1]],]
+	row.extend(calibration[node[0]].values())
+	outdata.append(row)
+
+dataprint.to_newfile(outfilename_base+'.calibration', outdata, overwrite=True)
+
