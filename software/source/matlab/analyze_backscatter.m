418eb392b9708fe6a0cd79d200a49e3b67f0e480
Ben Kempke <bpkempke@umich.edu>
2015-12-08 13:46:58 -0500

    modifying code to work with longer recordings at higher fidelity

--- software/source/matlab/analyze_backscatter.m
+++ software/source/matlab/analyze_backscatter.m
@@ -1,4 +1,6 @@
-fid = fopen('~/temp/polypoint/backscatter/backscatter_trace_exp8_110k_notag_wiredclk.csv','r');
+clear all
+
+fid = fopen('~/temp/polypoint/backscatter/backscatter_trace_exp8_110k_notag2_wiredclk.csv','r');
 
 %First line is useless.  Discard.
 fgetl(fid);
@@ -27,7 +29,7 @@ start_time = 0;
 end_time = 20;
 
 IMP_THRESH = 0.2;
-IMP_THRESH2 = 0.8;
+CHUNK_LEN = 100;
 CIR_LEN = 1016;
 INTERP_MULT = 100;
 
@@ -62,90 +64,116 @@ cir_data = cir_data(1:2:end,:) + 1i*cir_data(2:2:end,:);
 %CIRs are only 4064 octets long
 cir_data = cir_data(1:CIR_LEN,:);
 
-keyboard;
-
-%Interpolate the CIRs
-cir_data_fft = fft(cir_data,[],1).*repmat(fftshift(hamming(CIR_LEN)),[1,size(cir_data,2)]);
-cir_data_interp_fft = [cir_data_fft(1:CIR_LEN/2,:);zeros((INTERP_MULT-1)*CIR_LEN,num_cirs);cir_data_fft(CIR_LEN/2+1:end,:)];
-cir_data_interp = ifft(cir_data_interp_fft,[],1);
-
-%Come up with a rough estimate of ToA from each CIR
-toas = zeros(num_cirs,1);
-for ii=1:num_cirs
-	above_thresh = find(abs(cir_data_interp(:,ii)) > max(abs(cir_data_interp(:,ii)))*IMP_THRESH);
-	toas(ii) = above_thresh(1);
-end
-
-%Rotate CIRs to place ToA at zero
-for ii=1:num_cirs
-	cir_data_interp(:,ii) = circshift(cir_data_interp(:,ii),-toas(ii))./sqrt(sum(abs(cir_data_interp(:,ii).^2)));
-end
-
 %Figure out what the sequence numbers for each CIR are
 seq_num_idxs = transaction_idxs(find(spi_mosi(transaction_idxs) == 17)) + 17;
 seq_num_idxs = seq_num_idxs(2:2:end-2); %First read is always just used to determine packet length, also remove the last one as it's likely incomplete
 seq_nums = spi_miso(seq_num_idxs) + 256*spi_miso(seq_num_idxs+1) + 256*256*spi_miso(seq_num_idxs+2) + 256*256*256*spi_miso(seq_num_idxs+3);
+	
+ones_mean = zeros(CIR_LEN*INTERP_MULT,1);
+zeros_mean = zeros(CIR_LEN*INTERP_MULT,1);
+ones_mean_chunks = zeros(CIR_LEN*INTERP_MULT,ceil(num_cirs/CHUNK_LEN));
+zeros_mean_chunks = zeros(CIR_LEN*INTERP_MULT,ceil(num_cirs/CHUNK_LEN));
+toas = zeros(num_cirs,1);
+loop_num = 1;
 
-%Separate CIRs into two bins depending on where in the sequence they came from
-pn_sequence = [ ...
-        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, ... 
-        0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, ... 
-        0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, ... 
-        0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, ... 
-        0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, ... 
-        1, 1, 1];
+num_cirs_ones = 0;
+num_cirs_zeros = 0;
 
-ones_mean = zeros(length(pn_sequence),size(cir_data_interp,1));
-zeros_mean = zeros(length(pn_sequence),size(cir_data_interp,1));
-seq_nums_mod = mod(seq_nums, length(pn_sequence));
-pn_idxs = seq_nums_mod+1;
-pn_zeros = cir_data_interp(:,find(mod(seq_nums,2) == 0));
-pn_ones = cir_data_interp(:,find(mod(seq_nums,2) == 1));
+for cir_num = 1:CHUNK_LEN:num_cirs
 
-%%Fine-tune the shift of each CIR to minimize magnitude difference
-%pn_zeros = shift_corr(pn_zeros, INTERP_MULT, 10);
-%pn_zeros = shift_corr(pn_zeros, INTERP_MULT, 0.1);
-%pn_ones = shift_corr(pn_ones, INTERP_MULT, 10);
-%pn_ones = shift_corr(pn_ones, INTERP_MULT, 0.1);
+	first_cir = cir_num;
+	last_cir = min(cir_num+CHUNK_LEN-1,num_cirs);
+	cur_num_cirs = last_cir-first_cir+1;
+	cur_seq_nums = seq_nums(first_cir:last_cir);
+	
+	%Interpolate the CIRs
+	cir_data_fft = fft(cir_data(:,first_cir:last_cir),[],1).*repmat(fftshift(hamming(CIR_LEN)),[1,cur_num_cirs]);
+	cir_data_interp_fft = [cir_data_fft(1:CIR_LEN/2,:);zeros((INTERP_MULT-1)*CIR_LEN,cur_num_cirs);cir_data_fft(CIR_LEN/2+1:end,:)];
+	cir_data_interp = ifft(cir_data_interp_fft,[],1);
+	
+	%Come up with a rough estimate of ToA from each CIR
+	cur_toas = zeros(cur_num_cirs,1);
+	for ii=1:cur_num_cirs
+		above_thresh = find(abs(cir_data_interp(:,ii)) > max(abs(cir_data_interp(:,ii)))*IMP_THRESH);
+		cur_toas(ii) = above_thresh(1);
+	end
+	toas(first_cir:last_cir) = cur_toas;
+	
+	%Rotate CIRs to place ToA at zero
+	for ii=1:cur_num_cirs
+		cir_data_interp(:,ii) = circshift(cir_data_interp(:,ii),-cur_toas(ii))./sqrt(sum(abs(cir_data_interp(:,ii).^2)));
+	end
+	
 
-for ii=1:size(pn_zeros,2)
-	%[~,max_peak] = max(abs(pn_zeros(:,ii)));
-	%pn_zeros(:,ii) = circshift(pn_zeros(:,ii),-max_peak);
-	pn_zeros(:,ii) = pn_zeros(:,ii).*exp(-1i*angle(pn_zeros(1,ii)));
+	%%Separate CIRs into two bins depending on where in the sequence they came from
+	%pn_sequence = [ ...
+	%        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, ... 
+	%        0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, ... 
+	%        0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, ... 
+	%        0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, ... 
+	%        0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, ... 
+	%        1, 1, 1];
+	%
+	%seq_nums_mod = mod(cur_seq_nums, length(pn_sequence));
+	%pn_idxs = seq_nums_mod+1;
+	pn_zeros = cir_data_interp(:,find(mod(cur_seq_nums,2) == 0));
+	pn_ones = cir_data_interp(:,find(mod(cur_seq_nums,2) == 1));
+	
+	%%Fine-tune the shift of each CIR to minimize magnitude difference
+	%pn_zeros = shift_corr(pn_zeros, INTERP_MULT, 10);
+	%pn_zeros = shift_corr(pn_zeros, INTERP_MULT, 0.1);
+	%pn_ones = shift_corr(pn_ones, INTERP_MULT, 10);
+	%pn_ones = shift_corr(pn_ones, INTERP_MULT, 0.1);
+	
+	for ii=1:size(pn_zeros,2)
+		%[~,max_peak] = max(abs(pn_zeros(:,ii)));
+		%pn_zeros(:,ii) = circshift(pn_zeros(:,ii),-max_peak);
+		pn_zeros(:,ii) = pn_zeros(:,ii).*exp(-1i*angle(pn_zeros(1,ii)));
+	end
+	for ii=1:size(pn_ones,2)
+		%[~,max_peak] = max(abs(pn_ones(:,ii)));
+		%pn_ones(:,ii) = circshift(pn_ones(:,ii),-max_peak);
+		pn_ones(:,ii) = pn_ones(:,ii).*exp(-1i*angle(pn_ones(1,ii)));
+	end
+
+	num_cirs_ones = num_cirs_ones + size(pn_ones,2);
+	num_cirs_zeros = num_cirs_zeros + size(pn_zeros,2);
+	
+	%ones_mean = mean(abs(pn_ones),2);
+	%zeros_mean = mean(abs(pn_zeros),2);
+	ones_mean = ones_mean + sum(pn_ones,2);
+	zeros_mean = zeros_mean + sum(pn_zeros,2);
+	ones_mean_chunks(:,loop_num) = sum(pn_ones,2)/size(pn_ones,2);
+	zeros_mean_chunks(:,loop_num) = sum(pn_zeros,2)/size(pn_zeros,2);
+
+	%%Scale (and shift) each CIR so that the 20% to 80% leading edge matches the first the closest
+	%last_int_index = find(ones_mean > max(ones_mean)*IMP_THRESH2);
+	%last_int_index = last_int_index(1);
+	%best_shift = 0;
+	%best_scale = 0;
+	%best_fit = Inf;
+	%for shift = -INTERP_MULT:INTERP_MULT
+	%	shifted_cir = circshift(ones_mean,shift);
+	%	shifted_cir = shifted_cir(1:last_int_index);
+	%	for scale_idx = 1:last_int_index
+	%		scale = shifted_cir(scale_idx)/zeros_mean(scale_idx);
+	%		cand_fit = sum((shifted_cir/scale-zeros_mean(1:last_int_index)).^2); %Base fit metric off sum of squares difference
+	%		if cand_fit < best_fit
+	%			best_fit = cand_fit;
+	%			best_shift = shift;
+	%			best_scale = scale;
+	%		end
+	%	end
+	%end
+	%ones_mean_new = circshift(ones_mean/best_scale,best_shift);
+	
+	good_ones = max(abs(pn_ones-repmat(ones_mean,[1,size(pn_ones,2)])),[],1) < 7e-3;
+	ones_mean_new = sum(pn_ones(:,find(good_ones)),2)/sum(good_ones);
+	good_zeros = max(abs(pn_zeros-repmat(zeros_mean,[1,size(pn_zeros,2)])),[],1) < 7e-3;
+	zeros_mean_new = sum(pn_zeros(:,find(good_zeros)),2)/sum(good_zeros);
+
+	loop_num = loop_num + 1;
 end
-for ii=1:size(pn_ones,2)
-	%[~,max_peak] = max(abs(pn_ones(:,ii)));
-	%pn_ones(:,ii) = circshift(pn_ones(:,ii),-max_peak);
-	pn_ones(:,ii) = pn_ones(:,ii).*exp(-1i*angle(pn_ones(1,ii)));
-end
-ones_mean = mean(abs(pn_ones),2);
-zeros_mean = mean(abs(pn_zeros),2);
-ones_mean = sum(pn_ones,2)/size(pn_ones,2);
-zeros_mean = sum(pn_zeros,2)/size(pn_zeros,2);
 
-%%Scale (and shift) each CIR so that the 20% to 80% leading edge matches the first the closest
-%last_int_index = find(ones_mean > max(ones_mean)*IMP_THRESH2);
-%last_int_index = last_int_index(1);
-%best_shift = 0;
-%best_scale = 0;
-%best_fit = Inf;
-%for shift = -INTERP_MULT:INTERP_MULT
-%	shifted_cir = circshift(ones_mean,shift);
-%	shifted_cir = shifted_cir(1:last_int_index);
-%	for scale_idx = 1:last_int_index
-%		scale = shifted_cir(scale_idx)/zeros_mean(scale_idx);
-%		cand_fit = sum((shifted_cir/scale-zeros_mean(1:last_int_index)).^2); %Base fit metric off sum of squares difference
-%		if cand_fit < best_fit
-%			best_fit = cand_fit;
-%			best_shift = shift;
-%			best_scale = scale;
-%		end
-%	end
-%end
-%ones_mean_new = circshift(ones_mean/best_scale,best_shift);
-
-good_ones = max(abs(pn_ones-repmat(ones_mean,[1,size(pn_ones,2)])),[],1) < 7e-3;
-ones_mean_new = sum(pn_ones(:,find(good_ones)),2)/sum(good_ones);
-good_zeros = max(abs(pn_zeros-repmat(zeros_mean,[1,size(pn_zeros,2)])),[],1) < 7e-3;
-zeros_mean_new = sum(pn_zeros(:,find(good_zeros)),2)/sum(good_zeros);
-
+ones_mean = ones_mean/num_cirs_ones;
+zeros_mean = zeros_mean/num_cirs_zeros;
