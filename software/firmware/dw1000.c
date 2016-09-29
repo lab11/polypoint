9d6d4a6f0f3cec13fd0f51486d84b92655337466
Ben Kempke <bpkempke@umich.edu>
2016-09-29 15:54:07 -0400

    better reliability.  Still not quite there...

--- software/firmware/dw1000.c
+++ software/firmware/dw1000.c
@@ -744,6 +744,7 @@ dw1000_err_e dw1000_configure_settings () {
 	                 DWT_INT_RFCE |
 	                 DWT_INT_RFSL |
 	                 DWT_INT_RFTO |
+	                 DWT_INT_RXOVRR |
 	                 DWT_INT_RXPTO |
 	                 DWT_INT_SFDT |
 	                 DWT_INT_ARFE, 1);
