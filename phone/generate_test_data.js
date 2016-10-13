d09ab85a21be6633b7192666371804f0bc8b2826
Pat Pannuto <pat.pannuto@gmail.com>
2016-10-12 20:22:36 -0400

    Add test data script

---
+++ phone/generate_test_data.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+var request = require('request');
+POST_URL = null
+//POST_URL = "http://requestb.in/sxw295sx"
+
+if (POST_URL == null) {
+	console.log("ERROR: Edit file to supply POST_URL\n");
+	process.exit(-1);
+}
+
+function get_meta (id) {
+	return {
+		received_time: new Date().toISOString(),
+		device_id: id,
+		device_type: "surepoint_tag",
+	}
+}
+
+function get_value (beg, end) {
+	return Math.random()*(end-beg+1)+beg
+}
+
+var x = 5;
+var y = 5;
+
+function generate_surepoint_packet (id) {
+	x = x + get_value(-.5, .5);
+	y = y + get_value(-.5, .5);
+
+	// Reasonable bounds
+	if (x>20) x = 19.5;
+	if (y>20) y = 19.5;
+	if (x<0) x = .5;
+	if (y<0) y = .5;
+
+	return {
+		_meta: get_meta(id),
+		x: x,
+		y: y,
+	};
+}
+
+function publish (f, id) {
+	var rand = get_value(0, 100)
+
+	// Drop 10% of packets
+	if (rand >= 10) {
+		var pkt = f(id)
+
+		console.log(pkt)
+		var options = {
+			uri: POST_URL,
+			method: 'POST',
+			json: pkt,
+		};
+		request(options);
+	}
+}
+
+// Generate each packet at varying rates.
+setInterval(function () {
+	publish(generate_surepoint_packet, 'c0:98:e5:45:00:01')
+}, 1000)
+setInterval(function () {
+	publish(generate_surepoint_packet, 'c0:98:e5:45:00:02')
+}, 1000)
+setInterval(function () {
+	publish(generate_surepoint_packet, 'c0:98:e5:45:00:03')
+}, 1000)
+setInterval(function () {
+	publish(generate_surepoint_packet, 'c0:98:e5:45:00:04')
+}, 1000)
+
