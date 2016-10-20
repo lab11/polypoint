4c20eeb919c0bc2e2eee8314e98b641c526fdebe
Ben Kempke <bpkempke@umich.edu>
2016-10-20 14:12:10 -0400

    python script seems to now work correctly.  oct2py still giving errors though

---
+++ software/source/matlab/test_EM_GMMDS.m
@@ -0,0 +1,134 @@
+function [xhat_MDS, LOS_Matrix] = test_EM_GMMDS(D_hat, xy)
+
+prm.n = size(D_hat,1);     % number of nodes
+prm.d = 3;      % dimension
+%prm.conn = 18;   % network connectivity
+prm.T = size(D_hat,3);    % number of measurements
+prm.plos = 0.3;
+prm.k = 3;
+prm.mu      = [0    2   2.4];
+prm.sigma   = [0.2  0.4 0.3];
+prm.alpha   = [0.5  0.3 0.2];
+
+A = sum(D_hat > 0, 3) > 0;
+prm.conn = sum(A(:));
+
+% Generate network with required connectivity
+%[xy, A] = create_graph(prm.n,prm.d,prm.conn);
+
+% Sample measurements from the network with the required plos 
+%[ LOS_Matrix,alpha_true,mu_true,sigma_true,D_true,D_hat] = initSimDistr(prm,xy,A);
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Make the measurement matrix symmetric (IMPORTANT!)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 1 : size(D_hat,3)
+    D_hat(:,:,i) = nx_adjustDMatrix(D_hat(:,:,i));
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialize the EM-GMMDS algorithm
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+xy_hat = xy + 14*randn(size(xy)); 
+rep_CD = 3;
+rep_EM = 50;
+rep_MDS = 500;
+rep_MDSi = 20;
+
+
+% Run EM-GMMDS algorithm
+tic;
+[xhat,ahat,mhat,shat] = EM_GMMDS(D_hat,xy_hat,A,prm.k,rep_CD,rep_EM,rep_MDS,rep_MDSi);
+toc;
+
+
+% MDS
+tic;
+xhat_MDS = nx_MDS(D_hat,xy_hat,A,rep_CD*rep_MDS,rep_MDSi);
+toc;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Compute errors
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+RMSE = zeros(rep_CD*rep_MDS,1);
+for rep = 1:rep_CD*rep_MDS
+    [~,xy_hat] = procrustes(xy,xhat(:,:,rep), 'Scaling',false);
+    RMSE(rep)= sqrt(mean(sum((xy-xy_hat).^2,2)));
+end
+
+% KLDE = zeros(rep_CD*rep_EM,1);
+% for rep = 1:rep_CD*rep_EM
+%     pt = 0;
+%     for i = 1:prm.n
+%         for j = (i+1):prm.n
+%             if( A(i,j) == 1)
+%                 err = gaussmixk(permute(mu_true(i,j,:),[3 1 2]),...
+%                              permute(sigma_true(i,j,:),[3,1,2]),...
+%                              permute(alpha_true(i,j,:),[3,1,2]), ...   
+%                              permute(mhat(i,j,:,rep),[3 1 2 4]),...
+%                              permute(shat(i,j,:,rep),[3 1 2 4]),...
+%                              permute(ahat(i,j,:,rep),[3 1 2 4]));
+%                 KLDE(rep) = (pt*KLDE(rep) + abs(err))/(pt+1);
+%                 pt = pt+1;
+%             end
+%         end
+%     end
+% end
+
+RMSE_MDS = zeros(rep_CD*rep_MDS,1);
+for rep = 1:rep_CD*rep_MDS
+    [~,xy_hat] = procrustes(xy,xhat_MDS(:,:,rep), 'Scaling',false);
+    RMSE_MDS(rep)= sqrt(mean(sum((xy-xy_hat).^2,2)));
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Display
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%[~,xy_hat_proc] = procrustes(xy,xhat(:,:,end), 'Scaling',false);
+%[~,xy_hat_MDS_proc] = procrustes(xy,xhat_MDS(:,:,end), 'Scaling',false);
+%figure('position',[100 100 600 400],'color',[1 1 1]);
+%plot3(xy(:,1),xy(:,2),xy(:,3),'b+','MarkerSize',8,'LineWidth',3,'MarkerEdgeColor',[0 0.5 0.5]);
+%hold on;
+%plot3(xy_hat_proc(:,1),xy_hat_proc(:,2),xy_hat_proc(:,3),'ro','MarkerSize',6,'LineWidth',2);
+%plot3(xy_hat_MDS_proc(:,1),xy_hat_MDS_proc(:,2),xy_hat_MDS_proc(:,3),'mx','MarkerSize',6,'LineWidth',2);
+%%lines
+%for i= 1:prm.n
+%    for j = (i+1) : prm.n
+%        if A(i,j) == 1
+%            if( LOS_Matrix(i,j) == 1)
+%                plot3( [ xy(i,1), xy(j,1)],[ xy(i,2), xy(j,2)],[ xy(i,3), xy(j,3)],'k-');
+%            else
+%                plot3( [ xy(i,1), xy(j,1)],[ xy(i,2), xy(j,2)],[ xy(i,3), xy(j,3)],'k--');
+%            end
+%        end
+%    end
+%end
+%hold off
+%zlabel('meters');
+%grid on
+%legend('Ground Truth','GMMDS','MDS')
+%axis equal
+%
+%
+%figure('position',[700 100 600 400],'color',[1 1 1])
+%plot(RMSE,'r','linewidth',2)
+%xlabel('GMMDS Iterations')
+%ylabel('RMSE [m]')
+%hold on
+%plot(RMSE_MDS,'b','linewidth',2)
+%legend('GM-MDS','MDS');
+
+ 
+% figure('color',[1 1 1])
+% plot(KLDE,'b-','linewidth',2)
+% xlabel('EM Iterations')
+% ylabel('KL Divergece') 
+
+
---
+++ software/source/python/gmmds_demo.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import json
+import math
+import struct
+import serial
+import sys
+import os
+import binascii
+import argparse
+from oct2py import octave
+import scipy.io as sio
+
+def tic():
+    #Homemade version of matlab tic and toc functions
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
+
+def toc():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        return time.time() - startTime_for_tictoc
+    else:
+        print("Toc: start time not set")
+
+#Set up paths to point to python code directory
+octave.addpath('../matlab')
+octave.addpath('../matlab/gmmds_mwe')
+octave.addpath('../matlab/gmmds_mwe/EM')
+octave.addpath('../matlab/gmmds_mwe/MDS')
+octave.addpath('../matlab/gmmds_mwe/sim')
+octave.addpath('../matlab/gmmds_mwe/util')
+octave.eval('more off')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--serial',   default='/dev/tty.usbserial-AL00EZAS')
+parser.add_argument('-b', '--baudrate', default=3000000, type=int)
+parser.add_argument('-o', '--outfile',  default='out')
+
+args = parser.parse_args()
+
+dev = serial.Serial(args.serial, args.baudrate)
+if dev.isOpen():
+	print("Connected to device at " + dev.portstr)
+else:
+	raise NotImplementedError("Failed to connect to serial device " + args.serial)
+
+
+def useful_read(length):
+	b = dev.read(length)
+	while len(b) < length:
+		b += dev.read(length - len(b))
+	assert len(b) == length
+	return b
+
+
+HEADER      = (0x80018001).to_bytes(4, 'big')
+
+def find_header():
+	b = useful_read(len(HEADER))
+	while b != HEADER:
+		b = b[1:len(HEADER)] + useful_read(1)
+
+anchor_coords = {
+	'31': (  2.421, -1.032, 1.224),
+	'2e': (  4.598, -2.309, 0.740),
+	'26': (  4.598, -4.285, 1.983),
+	'22': (  2.476, -7.179, 1.882),
+	'27': (  0.055, -4.903, 1.482),
+	'3f': (  0.055, -3.904, 1.779),
+	'23': (  0.055, -2.657, 2.202),
+	'2b': (  0.055, -0.871, 2.263),
+	'2c': ( -0.188, -4.124, 2.433),
+	'37': ( -2.099, -4.557, 2.241),
+	'1f': ( -2.099, -2.594, 0.483),
+	'38': ( -0.133, -0.631, 1.549),
+	'3e': (  0.410,  0.206, 2.283),
+}
+
+good = 0
+bad = 0
+D_hat = np.empty([0, 0, 0])
+D_hat_len = np.empty([0, 0])
+D_hat_ids = {}
+tic()
+try:
+	while True:
+		sys.stdout.write("\rGood {}    Bad {}\t\t".format(good, bad))
+
+		try:
+			find_header()
+
+			reporting_id = binascii.hexlify(useful_read(1)).decode('utf-8')
+			ranging_id = binascii.hexlify(useful_read(1)).decode('utf-8')
+
+			if not reporting_id in D_hat_ids:
+				D_hat_ids[reporting_id] = len(D_hat_ids.keys())
+			if not ranging_id in D_hat_ids:
+				D_hat_ids[ranging_id] = len(D_hat_ids.keys())
+			reporting_idx = D_hat_ids[reporting_id]
+			ranging_idx = D_hat_ids[ranging_id]
+
+			for x in range(27):
+				cur_range = float(struct.unpack("<h", useful_read(2))[0])/1000
+				max_len = int(max(D_hat_len.flatten(), default=0)) + 1
+				D_hat.resize([len(D_hat_ids), len(D_hat_ids), max_len])
+				D_hat_len.resize([len(D_hat_ids), len(D_hat_ids)])
+				D_hat[reporting_idx,ranging_idx,int(D_hat_len[reporting_idx,ranging_idx])] = cur_range
+				D_hat_len[reporting_idx,ranging_idx] = D_hat_len[reporting_idx,ranging_idx] + 1
+
+			good += 1
+
+		except AssertionError:
+			bad += 1
+
+		#Re-perform network localization every second
+		if toc() > 5:
+			#Construct array of intial guesses for XY location
+			xy = np.zeros([D_hat.shape[0], 3])
+			for anchor_id in D_hat_ids:
+				xy[D_hat_ids[anchor_id],:] = np.asarray(anchor_coords[anchor_id])
+			print(xy)
+
+			xhat_MDS, LOS_Matrix = octave.test_EM_GMMDS(D_hat, xy)
+			D_hat = np.empty([0, 0])
+			D_hat_len = np.empty([0])
+			D_hat_ids = {}
+			tic()
+
+except KeyboardInterrupt:
+	pass
+   
+
